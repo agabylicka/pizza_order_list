b7bd4b8be9f3362cbc5b1ab4d3b8ae18eed28f7b
Agnieszka <aga_bylicka@poczta.onet.pl>
2021-07-25 22:00:42 +0200

    added new pizzas

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+import flask
+import json
+from flask import jsonify, request
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
 
 pizzas = [{"id": 2,
-           "name": 'margeritha',
+           "name": "Margarita",
            "price": 14.25},
           {"id": 3,
-           "name": 'hawaian',
+           "name": "Hawaiian",
            "price": 15.25
            }]
 
@@ -27,6 +30,31 @@ def to_order():
     return jsonify(pizzas)
 
 
+@app.route("/pizzas", methods=["POST"])
+def add_pizza():
+    print(request.json)
+    if request.is_json:
+        pizza = request.get_json()
+        pizza["id"] = next_id()
+        pizzas.append(pizza)
+        res = {'status': 'created'}
+        return jsonify(res)
+    else:
+        return jsonify("Request is not json", 400)
+
+
+def next_id() -> int:
+    if len(pizzas) == 0:
+        return 1
+    big = 1
+    for pizza in pizzas:
+        if pizza["id"] > big:
+            big = pizza["id"]
+    return big+1
+
+
 if __name__ == '__main__':
+    cos = next_id()
+    print(cos)
     app.debug = True
     app.run()
